4efc3fb0d6551fcec32a1bafce6086743773f631
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-03-17 04:44:48 -0300

    commit u1_exercicio3

---
+++ u1_exercicio3/cadpessoas_marcos/src/main/java/br/com/marcos/cadpessoas/cadpessoas_marcos/CadpessoasMarcosApplication.java
@@ -0,0 +1,14 @@
+package br.com.marcos.cadpessoas.cadpessoas_marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasMarcosApplication.class, args);
+
+	}
+
+}
---
+++ u1_exercicio3/cadpessoas_marcos/src/test/java/br/com/marcos/cadpessoas/cadpessoas_marcos/CadpessoasMarcosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.cadpessoas.cadpessoas_marcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasMarcosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

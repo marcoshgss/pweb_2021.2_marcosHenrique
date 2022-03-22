584c82d28add0b44065309169652313f621d795d
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-03-22 05:03:26 -0300

    novo commit

--- u1_exercicio3/cadpessoas_marcos/src/main/java/br/com/marcos/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/cadpessoas_marcos/src/main/java/br/com/marcos/cadpessoas/controllers/PessoaController.java
@@ -1,11 +1,13 @@
 package br.com.marcos.cadpessoas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import antlr.collections.List;
 import br.com.marcos.cadpessoas.model.Pessoa;
 import br.com.marcos.cadpessoas.repositories.PessoaRepository;
 
@@ -17,18 +19,18 @@ public class PessoaController {
 
     PessoaController(PessoaRepository pessoaR) {
         this.pessoaRepo = pessoaR;
-    }GetMapping
+    }@GetMapping
 
     public String inicio(){
         return "index.html";
     }
 
-    GetMapping("/listarPessoas")
+    @GetMapping("/listarPessoas")
 
     public ModelAndView listarPessoas() {
-        List<Pessoa> todasAsPessoas = pessoaRepo.findAll();
+        List <Pessoa> todasAsPessoas = pessoaRepo.findAll();
 
-        ModelAndView modelAndView = new ModelAndView(listarPessoas);
+        ModelAndView modelAndView = new ModelAndView("listarPessoas");
 
         modelAndView.addObject("todasAsPessoas", todasAsPessoas);
 

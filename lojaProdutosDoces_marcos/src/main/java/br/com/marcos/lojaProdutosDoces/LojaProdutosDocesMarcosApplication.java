2a0f940d81dc45538bef0bd290bc1de9d6e450da
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-05-14 07:50:32 -0300

    commit

---
+++ lojaProdutosDoces_marcos/src/main/java/br/com/marcos/lojaProdutosDoces/LojaProdutosDocesMarcosApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos.lojaProdutosDoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaProdutosDocesMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaProdutosDocesMarcosApplication.class, args);
+	}
+
+}

a77b166eee5b2cf5a787c064bc2b0248b9ce3815
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-05-14 13:44:46 -0300

    commit

--- lojaProdutosDoces_marcos/src/main/java/br/com/marcos/lojaProdutosDoces/controllers/ClienteController.java
+++ lojaProdutosDoces_marcos/src/main/java/br/com/marcos/lojaProdutosDoces/controllers/ClienteController.java
@@ -1,10 +1,15 @@
 package br.com.marcos.lojaProdutosDoces.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.marcos.lojaProdutosDoces.model.Cliente;
 import br.com.marcos.lojaProdutosDoces.repositories.ClienteRepository;
 
 @Controller
@@ -17,4 +22,30 @@ public class ClienteController {
     public String inicio() {
         return "index.html";
     }
+
+    @GetMapping("/listarClientes")
+
+    public ModelAndView listarClientes() {
+        List<Cliente> todasOsclientes = clienteRepo.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("listarClientes");
+
+        modelAndView.addObject("todasOsclientes", todasOsclientes);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/adicionarCliente")
+    public ModelAndView formAdicionarCliente() {
+        ModelAndView modelAndView = new ModelAndView("adicionarCliente");
+        modelAndView.addObject(new Cliente());
+        return modelAndView;
+    }
+
+    @PostMapping("/adicionarCliente")
+    public String adicionarCliente(Cliente c) {
+        this.clienteRepo.save(c);
+        return "redirect:/listarClientes";
+    }
+
 }

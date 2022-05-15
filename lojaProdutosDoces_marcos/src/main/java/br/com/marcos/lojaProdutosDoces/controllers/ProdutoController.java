fd4d32af1cf9e22485e922ce37a4d7f5f9a696b4
Marcos Henrique <sena_marcos.fp@hotmail.com>
2022-05-15 03:14:55 -0300

    commit

--- lojaProdutosDoces_marcos/src/main/java/br/com/marcos/lojaProdutosDoces/controllers/ProdutoController.java
+++ lojaProdutosDoces_marcos/src/main/java/br/com/marcos/lojaProdutosDoces/controllers/ProdutoController.java
@@ -1,10 +1,16 @@
 package br.com.marcos.lojaProdutosDoces.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.marcos.lojaProdutosDoces.model.Produto;
 import br.com.marcos.lojaProdutosDoces.repositories.ProdutoRepository;
 
 
@@ -15,15 +21,52 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtRepo;
 
-    @GetMapping
-    public String inicio() {
-        return "index.html";
+    @GetMapping("/listarProdutos")
+    
+    public ModelAndView listarProdutos() {
+        List<Produto> todosOsprodutos = produtRepo.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("listarProdutos");
+
+        modelAndView.addObject("todosOsprodutos", todosOsprodutos);
+
+        return modelAndView;
+    }
+    
+    @GetMapping("/adicionarProduto")
+    public ModelAndView formAdicionarProduto() {
+        ModelAndView modelAndView = new ModelAndView("adicionarProduto");
+        modelAndView.addObject(new Produto());
+        return modelAndView;
     }
 
-    
+    @PostMapping("/adicionarProduto")
+    public String adicionarProduto(Produto p) {
+        this.produtRepo.save(p);
+        return "redirect:/listarProdutos";
+    }
 
+    @GetMapping("/edit/{id}")
+    public ModelAndView formEditarProduto(@PathVariable("id") long id) {
+        Produto produto = produtRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+        ModelAndView modelAndView = new ModelAndView("editarProduto");
+        modelAndView.addObject(produto);
+        return modelAndView;
+    }
 
+    @PostMapping("/edit/{id}")
+    public ModelAndView editarProduto(@PathVariable("id") long id, Produto p) {
+        this.produtRepo.save(p);
+        return new ModelAndView("redirect:/listarProdutos");
+    }
 
-
+    @GetMapping("/remove/{id}")
+    public ModelAndView removerProduto(@PathVariable("id") long id) {
+        Produto aRemover = produtRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+                produtRepo.delete(aRemover);
+        return new ModelAndView("redirect:/listarProdutos");
+    }
 
 }
